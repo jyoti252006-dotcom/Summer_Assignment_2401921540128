8eb466e50135adc966ea484affa62d08ed91ad18
Jyoti <jyoti252006@gmail.com>
2026-06-01 22:52:10 +0530

    Refactor twoSum method to use HashMap for efficiency

--- LeetCode/Two-Sum/Solution.java
+++ LeetCode/Two-Sum/Solution.java
@@ -1,12 +1,13 @@
- for(var i =0;i<nums.length;i++){
-    var item = nums[i];   
-    for(var j=0; j < nums.length;j++){
-        var item2 = nums[j];
-        var sum = item + item2;
-        
-    if(sum === target ){
-      return [i,j];
-      
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      HashMap<Integer, Integer> map=new HashMap<>();
+      for(int i=0;i<nums.length;i++){
+        int complement=target-nums[i];
+        if(map.containsKey(complement)){
+            return new int[] {map.get(complement),i};
+        }
+        map.put(nums[i],i);
+      }
+     return new int[]{};
     }
-  }
- }
+}
