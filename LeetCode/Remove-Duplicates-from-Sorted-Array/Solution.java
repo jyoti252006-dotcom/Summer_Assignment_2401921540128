ad4c0ddfd418dd0550398fcdee8f81fc0b5550ca
Jyoti <jyoti252006@gmail.com>
2026-06-01 22:56:54 +0530

    Remove commented-out code in removeDuplicates method
    Removed commented-out code for expected nums array.

--- LeetCode/Remove-Duplicates-from-Sorted-Array/Solution.java
+++ LeetCode/Remove-Duplicates-from-Sorted-Array/Solution.java
@@ -1,7 +1,6 @@
 1class Solution {
 2    public int removeDuplicates(int[] nums) {
 3        int k=1;
-4        //int expectednums[]=new int[nums.length];
 5        for(int i=1;i<nums.length;i++){
 6            if(nums[i]!=nums[i-1]){
 7                nums[k++]=nums[i];
