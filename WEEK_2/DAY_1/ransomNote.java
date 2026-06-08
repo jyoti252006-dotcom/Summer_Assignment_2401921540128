9d570cc381fcbed45a7b5b4d93b1c4e85fa8e505
Jyoti <jyoti252006@gmail.com>
2026-06-08 20:42:15 +0530

    solution

---
+++ WEEK_2/DAY_1/ransomNote.java
@@ -0,0 +1,19 @@
+package WEEK_2.DAY_1;
+
+public class ransomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq=new int[26];
+        for(int i=0;i<magazine.length();i++){
+            char ch=magazine.charAt(i);
+            freq[ch-'a']++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            char ch=ransomNote.charAt(i);
+            if(freq[ch-'a']==0){
+                return false;
+            }
+            freq[ch-'a']--;
+        }
+        return true;
+    }
+}
