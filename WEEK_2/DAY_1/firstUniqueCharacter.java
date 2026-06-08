2066d055dac5cfa05c0b587d3ed5685dcc26e888
Jyoti <jyoti252006@gmail.com>
2026-06-08 20:30:45 +0530

    solution of firstUniqueCharacter

---
+++ WEEK_2/DAY_1/firstUniqueCharacter.java
@@ -0,0 +1,16 @@
+package WEEK_2.DAY_1;
+
+public class firstUniqueCharacter {
+    public int firstUniqChar(String s) {
+       int[] map=new int[26];
+       for(int i=0;i<s.length();i++){
+        map[s.charAt(i)-'a']++;
+       }
+       for(int i=0;i<s.length();i++){
+        if(map[s.charAt(i)-'a']==1){
+            return i;
+        }
+       }
+       return -1;
+    }
+}
