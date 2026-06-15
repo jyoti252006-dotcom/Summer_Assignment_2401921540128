a43164eb24f1c054cca6857209e8f8629bb87900
Jyoti <jyoti252006@gmail.com>
2026-06-15 23:03:39 +0530

    solution_3

---
+++ WEEK_3/DAY_1/MiddleOfLinkedList.java
@@ -0,0 +1,25 @@
+package WEEK_3.DAY_1;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { 
+        this.val = val; 
+    }
+    ListNode(int val, ListNode next) { 
+        this.val = val;
+        this.next = next; 
+    }
+}
+
+public class MiddleOfLinkedList {
+    public ListNode middListNode(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
