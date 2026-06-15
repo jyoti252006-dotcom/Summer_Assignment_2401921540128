63411a36db8de60e8009f004f0f4b195eb62f76f
Jyoti <jyoti252006@gmail.com>
2026-06-15 16:36:52 +0530

    linkedlist cycle solution

---
+++ WEEK_3/DAY_1/linkedListCycle.java
@@ -0,0 +1,23 @@
+package WEEK_3.DAY_1;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+public class linkedListCycle {
+    public boolean hasCycle(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
