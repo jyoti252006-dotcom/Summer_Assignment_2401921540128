f7c04b82c6ae4299c620b03208556c090bbadded
Jyoti <jyoti252006@gmail.com>
2026-06-16 18:00:04 +0530

    week2 day2 solution

---
+++ WEEK_3/DAY_2/MergeTwoSortedList.java
@@ -0,0 +1,33 @@
+package WEEK_3.DAY_2;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val; 
+    }
+    ListNode(int val, ListNode next) { 
+        this.val = val; 
+        this.next = next; 
+    }
+}
+
+public class MergeTwoSortedList {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        ListNode dummy=new ListNode(-1);
+        ListNode head=dummy;
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                head.next=list1;
+                list1=list1.next;
+            }
+            else{
+                head.next=list2;
+                list2=list2.next;
+            }
+            head=head.next;
+        }
+        head.next=(list1!=null)?list1:list2;
+        return dummy.next;
+    }
+}
