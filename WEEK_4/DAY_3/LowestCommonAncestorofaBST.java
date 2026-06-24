55dae429eef02ac1fb8d0eaa798303f8ec121311
Jyoti <jyoti252006@gmail.com>
2026-06-24 22:28:37 +0530

    week_4 day_3 solution

---
+++ WEEK_4/DAY_3/LowestCommonAncestorofaBST.java
@@ -0,0 +1,18 @@
+package WEEK_4.DAY_3;
+
+public class LowestCommonAncestorofaBST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } 
+            else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } 
+            else {
+                return root;
+            }
+        }
+        return null;
+    }
+}
---
+++ WEEK_4/DAY_3/ValidateBinarySearchTree.java
@@ -0,0 +1,20 @@
+package WEEK_4.DAY_3;
+
+public class ValidateBinarySearchTree {
+     public boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean validate(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        return validate(node.left, min, node.val) &&
+               validate(node.right, node.val, max);
+    }
+}
