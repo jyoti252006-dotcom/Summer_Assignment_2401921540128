1b5e6d68fe59d9c3939966cd2d8918203392e9c8
Jyoti <jyoti252006@gmail.com>
2026-06-25 20:44:11 +0530

    week_4 day_4 solution

---
+++ WEEK_4/DAY_4/pathSum.java
@@ -0,0 +1,30 @@
+package WEEK_4.DAY_4;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { 
+        this.val = val; 
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class pathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+
+        // leaf node
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val)
+            || hasPathSum(root.right, targetSum - root.val);
+    }
+}
