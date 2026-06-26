68ad1c66b498e98997fd241c6dd64cb3b3b9114c
Jyoti <jyoti252006@gmail.com>
2026-06-26 23:36:23 +0530

    week_4 day_5 solution

---
+++ WEEK_4/DAY_5/SerializeandDeserializeBinaryTree.java
@@ -0,0 +1,49 @@
+package WEEK_4.DAY_5;
+
+public class SerializeandDeserializeBinaryTree {
+     public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        preorder(root, sb);
+        return sb.toString();
+    }
+
+    private void preorder(TreeNode root, StringBuilder sb) {
+
+        if (root == null) {
+            sb.append("#,");
+            return;
+        }
+
+        sb.append(root.val).append(",");
+
+        preorder(root.left, sb);
+        preorder(root.right, sb);
+    }
+
+    // Deserialize
+    private int index = 0;
+
+    public TreeNode deserialize(String data) {
+
+        String[] arr = data.split(",");
+        index = 0;
+
+        return build(arr);
+    }
+
+    private TreeNode build(String[] arr) {
+
+        if (arr[index].equals("#")) {
+            index++;
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(arr[index++]));
+
+        root.left = build(arr);
+
+        root.right = build(arr);
+
+        return root;
+    }
+}
